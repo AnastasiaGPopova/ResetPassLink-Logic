3bdd101b0254a86d4692621d86178de1fe5b6807
AnastasiaGPopova <ana.popova.register@gmail.com>
2023-10-28 11:48:49 +0300

    Update mailService.js

--- mailService.js
+++ mailService.js
@@ -1,7 +1,53 @@
 const nodemailer = require('nodemailer')
 const {google} = require('googleapis')
 
-const CLIENT_ID = ""
-const CLIENT_SECRET=""
-const REDIRECT_URI=""
-const REFRESH_TOKEN=""
+const CLIENT_ID = '491899038443-ck72imqt4pv1k45u5ste9b2n2tf9dagg.apps.googleusercontent.com'
+const CLIENT_SECRET= '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'
+const REDIRECT_URI= 'https://developers.google.com/oauthplayground'
+const REFRESH_TOKEN= '1//04ziOJDkVu8DaCgYIARAAGAQSNwF-L9IrxU-DNtIcDwpNZQbB93kGjJIuYr0itG_Pm5xs4mBKKAcLnF3TLSEMeDNMhFbsWkW2wGA'
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
+
+oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN})
+
+
+async function sendEmail() {
+
+    try {
+
+        const accessToken = await oAuth2Client.getAccessToken()
+        const transport = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                type: "OAuth2",
+                user: "ana.popova.register@gmail.com",
+                clientId: CLIENT_ID,
+                client_secret: CLIENT_SECRET,
+                refreshToken: REFRESH_TOKEN,
+                accessToken: accessToken
+            }
+        })
+
+
+        const mailOptions = {
+            from: "ana.test@gmail",
+            to: "ana.popovaaa@gmail.com",
+            subject: "Hello From App",
+            text: "Hey, did it work?",
+            html:"<h1>Hey, did it work?</h1>"
+        }
+
+        const result = await transport.sendMail(mailOptions)
+        return result
+        
+    } catch (error) {
+        console.log(error.message)
+
+        
+    }
+
+}
+
+
+sendEmail().then((result) => console.log('Email sent...', result))
+           .catch((error) => console.log(error.message));
